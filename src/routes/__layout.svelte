a32569a11521c2188ad372a4525389273b52e9d7
Baptiste <baptiste.jouin44@gmail.com>
2022-01-17 15:21:22 +0000

    CSS styling of the application

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<script>
+    import "../app.css";
+</script>
+
+<main>
+    <slot />
+</main>
+<!--
+Si nous avons besoin d'un footer, il serait présent ci-dessous
+
+<footer>
+    &copy; {new Date().getFullYear()}, Someone.
+</footer>
+-->
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,10 +8,43 @@
     <title>{title}</title>
 </svelte:head>
 
-<h1>{title}</h1>
+<div class="todos">
+    <h1>{title}</h1>
 
-<form action="" method="">
-    <input type="text" name="todo" aria-label="Ajouter une tâches à effectuer" placeholder="+ entrez une tâches à ajouter">
-</form>
+    <form class="new" action="" method="">
+        <input
+            type="text"
+            name="todo"
+            aria-label="Ajouter une tâches à effectuer"
+            placeholder="+ entrez une tâches à ajouter"
+        />
+    </form>
 
-<TodoItem/>
+    <TodoItem />
+</div>
+
+<style>
+    .todos {
+        width: 100%;
+        max-width: 600px;
+        margin: 4rem auto 0;
+    }
+    .new {
+        margin: 0 0 0.5rem;
+    }
+    .new input {
+        font-size: 28px;
+        border-radius: 28px;
+        width: 100%;
+        padding: 0.5em 1em 0.3em 1em;
+        text-align: center;
+        background: none;
+    }
+    .todos :global(input) {
+        border: 1px solid transparent;
+    }
+    .todos :global(input:focus-visible) {
+        box-shadow: inset 1px 1px 6px rgba(0, 0, 0, 0.1);
+        outline: none;
+    }
+</style>
