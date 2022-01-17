77a8ee4926984d7c3da95338442004174e6f32c2
Baptiste <baptiste.jouin44@gmail.com>
2022-01-17 11:45:09 +0000

    Develop HTML structure.

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,15 @@
+<div>
+    <form action="" method="">
+        <input type="hidden" name="done" value="">
+        <button aria-label="Définir comme fait ou non fait">Fait/NonFait</button>
+    </form>
+
+    <form action="" method="">
+        <input type="text">
+        <button aria-label="Sauvegarder les changements">Sauvegarder</button>
+    </form>
+
+    <form action="" method="">
+        <button aria-label="Supprimer les changements">Supprimer</button>
+    </form>
+</div>
